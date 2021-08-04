1656ddf14f95f5e5dbb7e620168cabbac8f10d7b
Pablo Bonilla <pbonilla29@hotmail.com>
2021-08-03 22:11:24 -0600

    Add header navigation

---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import { Switch, Route, Redirect } from "react-router-dom"
+
+import Home from './../pages/Home';
+import Shop from './../pages/Shop';
+
+const Main = () => {
+  return (
+    <main>
+      <Switch>
+        <Route path='/home' component={Home}></Route>
+        <Route path='/shop' component={Shop}></Route>
+        <Route path='/' exact>
+          <Redirect to='/home' />
+        </Route>
+        <Route path='*'>
+          <Redirect to='/404' />
+        </Route>
+      </Switch>
+    </main>
+  );
+};
+
+export default Main;
