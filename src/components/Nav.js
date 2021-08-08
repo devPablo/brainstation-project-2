a6526f6262d82b6ce538b1059f339ba788c3d538
Pablo Bonilla <pbonilla29@hotmail.com>
2021-08-07 21:56:34 -0600

    Add responsive nav menu animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 import { BsBag, BsList, BsX } from 'react-icons/bs';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../store/CartContext';
 
 const Nav = () => {
-  const [isMenuClosed, setIsMenuClosed] = useState(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const { setShowCart } = useContext(CartContext);
 
@@ -13,52 +13,96 @@ const Nav = () => {
   };
 
   const menuClosedHandler = () => {
-    setIsMenuClosed(!isMenuClosed);
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    navMenuHandler();
+  }, [isMenuOpen]);
+
+  const navMenuHandler = () => {
+    const navResponsive = document.getElementById('js-nav-responsive');
+    if (navResponsive !== undefined) {
+      if (isMenuOpen) {
+        navResponsive.style.top = '0px';
+      } else {
+        navResponsive.style.top = '-100vh';
+      }
+    }
   };
 
   return (
-    <nav className='nav'>
-      <ul className={`nav__list ${isMenuClosed ? '' : 'nav__list--open'}`}>
-        <li className='nav__list-item'>
-          <Link className='nav__link' to='/home' onClick={menuClosedHandler}>
-            Home
-          </Link>
-        </li>
-        <li className='nav__list-item'>
-          <Link
-            className='nav__link'
-            to='/shop'
-            onClick={!isMenuClosed ? menuClosedHandler : null}
-          >
-            Shop
-          </Link>
-        </li>
-        <li className='nav__list-item'>
-          <Link className='nav__link' to='/contact' onClick={menuClosedHandler}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+    <>
+      <nav className='nav'>
+        <ul className='nav__list'>
+          <li className='nav__list-item'>
+            <Link className='nav__link' to='/home'>
+              Home
+            </Link>
+          </li>
+          <li className='nav__list-item'>
+            <Link className='nav__link' to='/shop'>
+              Shop
+            </Link>
+          </li>
+          <li className='nav__list-item'>
+            <Link className='nav__link' to='/contact'>
+              Contact
+            </Link>
+          </li>
+        </ul>
 
-      <div className='nav__sections-wrapper'>
-        <span className='nav__title'>BONIV.</span>
-      </div>
-      <div className='nav__sections-wrapper'>
-        <p className='nav__username'>Pablo Bonilla</p>
-        <BsBag className='nav__icon nav__icon--bag' onClick={openCart} />
-        {isMenuClosed ? (
+        <div className='nav__sections-wrapper'>
+          <span className='nav__title'>BONIV.</span>
+        </div>
+        <div className='nav__sections-wrapper'>
+          <p className='nav__username'>Pablo Bonilla</p>
+          <BsBag className='nav__icon nav__icon--bag' onClick={openCart} />
           <BsList
             className='nav__icon nav__icon--closedmenu'
             onClick={menuClosedHandler}
           />
-        ) : (
+        </div>
+      </nav>
+
+      <div id='js-nav-responsive' className='nav-menu'>
+        <div className='nav-menu__header'>
           <BsX
-            className='nav__icon nav__icon--closedmenu'
+            className='nav-menu__icon-closedmenu'
             onClick={menuClosedHandler}
           />
-        )}
+        </div>
+        <ul className='nav-menu__list'>
+          <li className='nav-menu__list-item'>
+            <Link
+              className='nav-menu__link'
+              to='/home'
+              onClick={menuClosedHandler}
+            >
+              Home
+            </Link>
+          </li>
+          <li className='nav-menu__list-item'>
+            <Link
+              className='nav-menu__link'
+              to='/shop'
+              onClick={menuClosedHandler}
+            >
+              Shop
+            </Link>
+          </li>
+          <li className='nav-menu__list-item'>
+            <Link
+              className='nav-menu__link'
+              to='/contact'
+              onClick={menuClosedHandler}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </>
   );
 };
 
